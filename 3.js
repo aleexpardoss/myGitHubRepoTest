8f2a07d651a3074ed11f6cac2691df85956031c6
Alejandro Prados <alex@MacBook-Pro-de-Alejandro.local>
2025-10-01 14:07:00 +0200

    Añadiendo un documento

---
+++ 3.js
@@ -0,0 +1,4 @@
+let sueldo = prompt("¿Cúal es tu sueldo bruto?");
+let sueldoFinal;
+sueldoFinal = sueldo - sueldo*0.2;
+console.log ("Tu sueldo neto, con la reducción aplicada es de: " + sueldoFinal + " €");
